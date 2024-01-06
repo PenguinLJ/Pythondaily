b499065208c19c81a59db5f0312e2f3a3d0e7969
Administrator <jun59945@gmail.com>
2024-01-06 13:45:38 +0800

    1.6complete

--- 202401/cityroute.py
+++ 202401/cityroute.py
@@ -1,5 +1,11 @@
-paths = [['london', 'new york'], ['new york', 'lima'], ['lima', 'sao paulo']]
+"""
+给你一份旅游线路图，该线路图中的旅行线路用数组paths表示，
+其中paths[i]=[cityAi,cityBi]表示该线路将会从cityAi直接前往cityBi。
+请你找出这次旅行的终点站，即没有任何可以通往其他城市的线路的城市。
+题目数据保证线路图会形成一条不存在循环的线路，因此恰有一个旅行终点站。
+"""
 
+paths = [['london', 'new york'], ['new york', 'lima'], ['lima', 'sao paulo']]
 
 start = set()  # 创建一个集合用于存放出发城市
 
